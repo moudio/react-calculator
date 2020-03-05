b1a8e5c777001da23ddc14984be5ba2f78679367
Mouhamadou Diouf <mouhamadoudioufsn@gmail.com>
2020-03-05 11:47:41 +0000

    Correcting the linter errors

--- src/components/App.js
+++ src/components/App.js
@@ -9,15 +9,13 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-    }
+    };
   }
-
-
 
   render() {
     return (
       <div className="App">
-        <Display result="0"/>
+        <Display result="0" />
         <ButtonPanel />
       </div>
     );
