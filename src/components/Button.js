7a8b61f4e3e3618ceec2ca9681b4f1cc583ebe7b
Mouhamadou Diouf <mouhamadoudioufsn@gmail.com>
2020-03-05 11:10:48 +0000

    Fixing the linter errors

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,11 +2,9 @@ import React from 'react';
 import Proptypes from 'prop-types';
 import '../Button.css';
 
-const button = ({name, color, wide}) => {
-  return (
-    <button type="button" className="Button" style={{ background: color, width: wide === true ? '50%' : '25%' }}>{name}</button>
-  );
-};
+const button = ({ name, color, wide }) => (
+  <button type="button" className="Button" style={{ background: color, width: wide === true ? '50%' : '25%' }}>{name}</button>
+);
 
 button.defaultProps = {
   name: '',
