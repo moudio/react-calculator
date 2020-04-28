76bbf14e573f5400edb3b2bae5b1aaf5ae277d25
Mouhamadou Diouf <mouhamadoudioufsn@gmail.com>
2020-04-28 15:08:40 +0000

    Updates

---
+++ precache-manifest.c7128277be7dd545157b30a6d84a09d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76bd7eb4cdba569336875707430ff1f0",
+    "url": "/react_calculator/index.html"
+  },
+  {
+    "revision": "ceb25c648476bb5ce211",
+    "url": "/react_calculator/static/css/main.6ea69bc6.chunk.css"
+  },
+  {
+    "revision": "4afb5cd16fac3157c885",
+    "url": "/react_calculator/static/js/2.a4522965.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react_calculator/static/js/2.a4522965.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ceb25c648476bb5ce211",
+    "url": "/react_calculator/static/js/main.c1d3245f.chunk.js"
+  },
+  {
+    "revision": "a2a4dbd3ecf630eee1d2",
+    "url": "/react_calculator/static/js/runtime-main.7bd9df88.js"
+  }
+]);
